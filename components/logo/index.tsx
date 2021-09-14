5f81944518d935a769a314bbe8df7827d78401b1
d-ivashchuk <dimaiv@me.com>
2021-09-14 12:43:16 +0200

    add dark logo for the platform

--- components/logo/index.tsx
+++ components/logo/index.tsx
@@ -1,6 +1,5 @@
 import {
-  Box,
-  Heading,
+  useColorMode,
   HTMLChakraProps,
   Stack,
   useMediaQuery,
@@ -12,12 +11,12 @@ type ExtendedProps = { preserveText?: boolean };
 type Props = HTMLChakraProps<"svg"> & ExtendedProps;
 
 export const Logo = (props: Props) => {
-  const [isSmallerThan600px] = useMediaQuery("(max-width: 600px)");
+  const { colorMode } = useColorMode();
 
   return (
     <Stack isInline alignItems="center">
       <Image
-        src="/logo.svg"
+        src={colorMode === "dark" ? "/dark-logo.svg" : "/logo.svg"}
         alt="Picture of the author"
         width={140}
         height={60}
